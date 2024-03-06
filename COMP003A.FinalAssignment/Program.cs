ae8b3ae6a1a0448b29926577fc5e89ff46a4ef59
Diego Carmona <ultgojifan@gmail.com>
2024-03-05 23:40:16 -0800

    Add project files.

---
+++ COMP003A.FinalAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Author: Diego Caarmona
+ * Course: COMP003A
+ * Purpose: A culmination of all the skills and praactice garnered during the 9 week course of development with C#. To show and be jugded baased on my klnowlegde and talent with the C# language.
+ */
+
+namespace COMP003A.FinalAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+
+    }
+}
